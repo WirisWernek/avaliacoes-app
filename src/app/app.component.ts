5415156e14c9d316dd0e106ec7da13d510af7f89
Wiris Wernek <rafaeljwerneck43@gmail.com>
2023-11-04 00:52:07 -0300

    feat: add estilização do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Firestore, collectionData } from '@angular/fire/firestore';
 import { collection } from 'firebase/firestore';
+import { initFlowbite } from 'flowbite';
 import { Observable } from 'rxjs';
 
 interface AvaliacaoModel {
@@ -13,7 +14,7 @@ interface AvaliacaoModel {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'avaliacoes';
   firestore: Firestore = inject(Firestore);
   itemCollection = collection(this.firestore, 'reviews');
@@ -21,5 +22,9 @@ export class AppComponent {
 
   constructor(){
 	this.avaliacoes$ = collectionData(this.itemCollection);
+  }
+
+  ngOnInit(): void {
+    initFlowbite();
   }
 }
